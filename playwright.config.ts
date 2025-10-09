328db188ca1c2408732917c1a5346b6625dd0ef1
prazumeika <s34086@pjwstk.edu.pl>
2025-10-09 18:51:54 +0200

    added 2 files with tests: task1.spec.ts and task2.spec.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './srs/tests',
+  testDir: './src/tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -36,18 +36,18 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], headless: true },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ src/tests/demo/task2.spec.ts
@@ -0,0 +1,96 @@
+import test, { expect } from "@playwright/test";
+
+interface ICredentials {
+  firstname: string;
+  lastname: string;
+  address: string;
+  email: string;
+  phone: string;
+  country: string;
+  gender: string;
+  hobbies: string[];
+  language: string;
+  skills: string[];
+  dateOfBirth: {
+    year: number;
+    month: string;
+    day: number;
+  };
+  password: string;
+  passwordConfirmation: string;
+}
+
+test.describe("[Demo-registration-form][Detailed registration form]", () => {
+  const validCredentials: ICredentials = {
+    firstname: "Mary",
+    lastname: "Jackson",
+    address: "Amster avenue 6b",
+    email: "mary.jackson@gmail.com",
+    phone: "123456789",
+    country: "UK",
+    gender: "female",
+    hobbies: ["Travelling", "Movies", "Gaming"],
+    language: "English",
+    skills: ["JavaScript", "Python"],
+    dateOfBirth: {
+      year: 1980,
+      month: "May",
+      day: 31,
+    },
+    password: "SuperSecret",
+    passwordConfirmation: "SuperSecret",
+  };
+
+  test.beforeEach(async ({ page }) => {
+    const url = "https://anatoly-karpovich.github.io/demo-registration-form/";
+    await page.goto(url);
+  });
+
+  test("Registration with valid inputs for all fields", async ({ page }) => {
+    const fistNameInput = page.locator("#firstName");
+    const lastNameInput = page.locator("#lastName");
+    const adressInput = page.locator("#address");
+    const emailInput = page.locator("#email");
+    const phoneInput = page.locator("#phone");
+    const countryDropdown = page.locator("#country");
+    const genderRadio = page.locator('//*[contains(@value, "female")]');
+    const languageInput = page.locator("#language");
+    const skillsForm = page.locator("#skills");
+    const yearOfBirthDropdown = page.locator("#year");
+    const monthOfBirthDropdown = page.locator("#month");
+    const dayOfBirthDropdown = page.locator("#day");
+    const passwordInput = page.locator("#password");
+    const passwordConfirmationInput = page.locator("#password-confirm");
+    const submitButton = page.locator('button[type="submit"]');
+    const fullNameConfirmation = page.locator("#fullName");
+
+    await fistNameInput.fill(validCredentials.firstname);
+    await lastNameInput.fill(validCredentials.lastname);
+    await adressInput.fill(validCredentials.address);
+    await emailInput.fill(validCredentials.email);
+    await phoneInput.fill(validCredentials.phone);
+    await countryDropdown.selectOption({ value: validCredentials.country });
+    await genderRadio.check();
+
+    for (const hobby of validCredentials.hobbies) {
+      await page.locator(`input.hobby[value="${hobby}"]`).check();
+    }
+
+    await languageInput.fill(validCredentials.language);
+    await skillsForm.selectOption(validCredentials.skills);
+    await yearOfBirthDropdown.selectOption(
+      validCredentials.dateOfBirth.year.toString()
+    );
+    await monthOfBirthDropdown.selectOption(validCredentials.dateOfBirth.month);
+    await dayOfBirthDropdown.selectOption(
+      validCredentials.dateOfBirth.day.toString()
+    );
+    await passwordInput.fill(validCredentials.password);
+    await passwordConfirmationInput.fill(validCredentials.passwordConfirmation);
+    await submitButton.click();
+
+    await expect(fullNameConfirmation).toContainText(
+      `${validCredentials.firstname} ${validCredentials.lastname}`
+    );
+  });
+});
