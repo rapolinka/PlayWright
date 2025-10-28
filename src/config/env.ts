3ebef04f533208d1fcf00d9073da2ecf13a7a6e2
prazumeika <s34086@pjwstk.edu.pl>
2025-10-28 12:00:36 +0100

    made a small change to env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import { ICredentials } from "data/types/credentils.types";
 import "dotenv/config";
 
-export const SALES_PORTAL_URL = process.env.SALES_PORTAL_URL;
+export const SALES_PORTAL_URL = process.env.SALES_PORTAL_URL!;
 export const credentials: ICredentials = {
   username: process.env.USER_NAME!,
   password: process.env.USER_PASSWORD!,
